36cde6a8c64e6217702c72a31584c03f6ed95e52
IgorYanko <99884866+IgorYanko@users.noreply.github.com>
2024-10-17 16:07:13 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const clientId = '1fd64948cbdd44d29396ce9aca70ee5d';
-const redirectUri = 'https://igoryanko.github.io/Spotify-Genius/';
+const redirectUri = 'https://igoryanko.github.io/Spotify-Genius/callback.html';
 const scopes = 'user-read-private user-read-email';
 const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
